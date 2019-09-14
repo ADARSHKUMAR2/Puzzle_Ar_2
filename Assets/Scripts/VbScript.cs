5c61f0476b1e8094097e9cb8d4757192ff952791
Adarsh <adarshkumar124@gmail.com>
2019-09-14 13:41:20 +0530

    another double click

--- Assets/Scripts/VbScript.cs
+++ Assets/Scripts/VbScript.cs
@@ -4,60 +4,71 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class VbScript : MonoBehaviour
+public class VbScript : MonoBehaviour 
 {
+    private bool isdouble = false;
+    private float doubleClickTimeLimit = 0.25f;
+    public GameObject Object_to_rotate;
 
-    private Touch touch;
-    private Vector2 touchPosition;
-
-    
-
-    int TapCount;
-    public float MaxDubbleTapTime;
-    float NewTime;
-
-    void Start()
+    protected void Start()
     {
-        TapCount = 0;
+        StartCoroutine(InputListener());
     }
 
-    void Update()
+    // Update is called once per frame
+    private IEnumerator InputListener()
     {
-        if (Input.touchCount == 1)
-        {
-            Touch touch = Input.GetTouch(0);
+        while (enabled)
+        { //Run as long as this is activ
 
-            if (touch.phase == TouchPhase.Ended)
-            {
-                TapCount += 1;
-            }
+            if (Input.GetMouseButtonDown(0))
+                yield return ClickEvent();
 
-            if (TapCount == 1)
-            {
-
-                NewTime = Time.time + MaxDubbleTapTime;
-            }
-            else if (TapCount == 2 && Time.time <= NewTime)
-            {
-
-                //Whatever you want after a dubble tap    
-                //print("Dubble tap");
-                float rotationX = Input.GetAxis("Mouse X") * 200f * Mathf.Deg2Rad;
-                transform.Rotate(Vector3.up, -rotationX);
-
-                TapCount = 0;
-            }
-
-        }
-        if (Time.time > NewTime)
-        {
-            TapCount = 0;
+            yield return null;
         }
     }
+
+    private IEnumerator ClickEvent()
+    {
+        //pause a frame so you don't pick up the same mouse down event.
+        yield return new WaitForEndOfFrame();
+
+        float count = 0f;
+        while (count < doubleClickTimeLimit)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                DoubleClick();
+                yield break;
+            }
+            count += Time.deltaTime;// increment counter by change in time between frames
+            yield return null; // wait for the next frame
+        }
+        SingleClick();
+    }
+
+
+    private void SingleClick()
+    {
+        isdouble = false;
+        Debug.Log("Single Click");
+    }
+
+    private void DoubleClick()
+    {
+        isdouble = true;
+        Debug.Log("Double Click");
+    }
+
+
 
 
     public void OnMouseDrag()
     {
+      
+            float rotationX = Input.GetAxis("Mouse X") * 200f * Mathf.Deg2Rad;
+            transform.Rotate(Vector3.up, -rotationX);
+       
         
     }
 
